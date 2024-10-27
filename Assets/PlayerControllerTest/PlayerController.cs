7e3f062918f5bd7512e713918530ce7919c58d7b
li lin <lovelyappleoffie@gmial.com>
2024-10-27 12:41:21 +0900

    Tester合併

---
+++ Assets/PlayerControllerTest/PlayerController.cs
@@ -0,0 +1,247 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Compression;
+using TreeEditor;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    public float GRAVITY;
+    public float JUMP_POWER;
+    public float BODY_SIZE_HALF;
+    public float HOR_INPUT_SPPED;
+    public float MAX_HOR_INPUT_SPEED;
+    public float DRAG_VALUE = 0.97f;
+    public float MIN_MOVE_POWER = 0.1f;
+
+
+    public Vector3 UserInputDicreionLocal;
+    public Vector3 CurrentInputMoveLocal;
+    public float CurrentMoveSpeed;
+    public Vector3 FinalVelocityWorld;
+    public Vector3 FinalVelocityWorldApply;
+
+    Transform _cmemraTransform;
+    public bool AllowRotate;
+    float _xRotaition;
+    public float MouseSensitivity;
+    private bool _isLoackedCursor;
+    void Awake()
+    {
+        _cmemraTransform = Camera.main.transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftCommand))
+        {
+            if (!_isLoackedCursor)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                _isLoackedCursor = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                _isLoackedCursor = false;
+            }
+        }
+        // 毎回初期化する
+        FinalVelocityWorld = Vector3.zero;
+
+
+        ///----- カメラ回転-----///
+        if (AllowRotate)
+        {
+            // マウスのX軸とY軸の入力を取得
+            var mouseSensitivity = MouseSensitivity;
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            // if (mouseX != 0 || mouseY != 0)
+            {
+                // 縦方向（Y軸）の回転を制限
+                _xRotaition -= mouseY;
+                _xRotaition = Mathf.Clamp(_xRotaition, -90f, 90f);  // 上下の視点回転を90度までに制限
+
+                // カメラの回転を上下方向（X軸）に適用
+                // _cmemraTransform.localRotation = Quaternion.Euler(_xRotaition, 0f, 0f);
+                // プレイヤーの体の回転を左右方向（Y軸）に適用
+                transform.Rotate(Vector3.up * mouseX);
+            }
+        }
+
+
+        // Input受付
+        if (Input.GetKey(KeyCode.W))
+        {
+            UserInputDicreionLocal.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            UserInputDicreionLocal.z = -1;
+        }
+        else
+        {
+            UserInputDicreionLocal.z = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            UserInputDicreionLocal.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            UserInputDicreionLocal.x = -1;
+        }
+        else
+        {
+            UserInputDicreionLocal.x = 0;
+        }
+
+        // Inoutを加速度に変換
+        if (UserInputDicreionLocal.x != 0)
+        {
+            if (CurrentInputMoveLocal.x * UserInputDicreionLocal.x < 0)
+            {
+                CurrentInputMoveLocal.x *= DRAG_VALUE;
+
+                if (CurrentInputMoveLocal.x * CurrentInputMoveLocal.x <= MIN_MOVE_POWER * MIN_MOVE_POWER)
+                {
+                    CurrentInputMoveLocal.x = 0;
+                }
+            }
+
+            CurrentInputMoveLocal.x += UserInputDicreionLocal.x * HOR_INPUT_SPPED * Time.deltaTime;
+        }
+        else
+        {
+            CurrentInputMoveLocal.x = 0;
+        }
+
+        if (UserInputDicreionLocal.z != 0)
+        {
+            if (CurrentInputMoveLocal.z * UserInputDicreionLocal.z < 0)
+            {
+                CurrentInputMoveLocal.z *= DRAG_VALUE;
+
+                if (CurrentInputMoveLocal.z * CurrentInputMoveLocal.z <= MIN_MOVE_POWER * MIN_MOVE_POWER)
+                {
+                    CurrentInputMoveLocal.z = 0;
+                }
+            }
+
+            CurrentInputMoveLocal.z += UserInputDicreionLocal.z * HOR_INPUT_SPPED * Time.deltaTime;
+        }
+        else
+        {
+            CurrentInputMoveLocal.z = 0;
+        }
+
+        // Input加速度の最大限を求める
+        if (CurrentInputMoveLocal.x != 0 || CurrentInputMoveLocal.z != 0)
+        {
+            CurrentInputMoveLocal.y = 0;
+            CurrentMoveSpeed = CurrentInputMoveLocal.sqrMagnitude;
+
+            var powerScale = MAX_HOR_INPUT_SPEED / CurrentMoveSpeed;
+
+            if (powerScale < 1)
+            {
+                CurrentInputMoveLocal.x *= powerScale;
+                CurrentInputMoveLocal.z *= powerScale;
+            }
+        }
+        else
+        {
+            CurrentMoveSpeed = 0;
+        }
+
+        // 自然運動中の摩擦力計算
+        // TODO 地面判定
+        if (UserInputDicreionLocal.x == 0 || (UserInputDicreionLocal.x * FinalVelocityWorld.x) < 0)
+        {
+            FinalVelocityWorld.x *= DRAG_VALUE;
+        }
+
+        if (UserInputDicreionLocal.z == 0 || (UserInputDicreionLocal.z * FinalVelocityWorld.z) < 0)
+        {
+            FinalVelocityWorld.z *= DRAG_VALUE;
+        }
+
+        // WorldDirecionに変換
+        if (CurrentMoveSpeed != 0)
+        {
+            FinalVelocityWorld = transform.TransformDirection(CurrentInputMoveLocal);
+        }
+
+        FinalVelocityWorldApply = FinalVelocityWorld * Time.deltaTime;
+
+        if (FinalVelocityWorldApply.x != 0 || FinalVelocityWorldApply.z != 0)
+        {
+
+        }
+
+        // 反映
+        if (FinalVelocityWorldApply.x != 0 || FinalVelocityWorldApply.y != 0 || FinalVelocityWorldApply.z != 0)
+        {
+            transform.position += FinalVelocityWorldApply;
+        }
+
+    }
+    [ContextMenu("AAAAA")]
+    public void OnEdtiroExecute()
+    {
+        var (fixDirec, normalDirect, hitPos) = GetHitPlaneVect(transform.forward * 5);
+
+        moveLine.SetPositions(new Vector3[] { transform.position, transform.TransformPoint(Vector3.forward * 5) });
+
+        if (fixDirec.HasValue)
+        {
+            fixLine.SetPositions(new Vector3[] { transform.position, transform.position + fixDirec.Value });
+        }
+        else
+        {
+            fixLine.SetPositions(new Vector3[] { Vector3.zero, Vector3.up });
+        }
+
+        if (normalDirect.HasValue && hitPos.HasValue)
+        {
+            hitNormalLine.SetPositions(new Vector3[] { hitPos.Value, hitPos.Value + normalDirect.Value * 5 });
+        }
+        else
+        {
+            fixLine.SetPositions(new Vector3[] { Vector3.zero, Vector3.up });
+        }
+    }
+    private (Vector3?, Vector3?, Vector3?) GetHitPlaneVect(Vector3 moveDirection)
+    {
+        const float HIT_CHECK_FLOAT_DISTANCE = 0.05f;
+        moveDistance = Mathf.Sqrt(moveDirection.x * moveDirection.x + moveDirection.z * moveDirection.z);
+        var direction = new Vector3(moveDirection.x, 0, moveDirection.z).normalized;
+        var castDistance = moveDistance + HIT_CHECK_FLOAT_DISTANCE;
+        if (Physics.SphereCast(transform.position, BODY_SIZE_HALF, direction, out var hitInfo, castDistance))
+        {
+            var hitOriginPoint = hitInfo.point + hitInfo.normal * (HIT_CHECK_FLOAT_DISTANCE + BODY_SIZE_HALF);
+            hitCheckMovedDistance = Vector3.Distance(transform.position, hitOriginPoint);
+
+            if (hitCheckMovedDistance < moveDistance)
+            {
+                var newVelocityWorld = direction * hitCheckMovedDistance;
+                float dotProduct = Vector3.Dot(direction, hitInfo.normal);
+                Vector3 projection = dotProduct * hitInfo.normal;
+                newVelocityWorld += projection * hitCheckMovedDistance;
+                return (newVelocityWorld, hitInfo.normal, hitOriginPoint);
+            }
+        }
+
+        return (null, null, null);
+    }
+    public LineRenderer moveLine;
+    public LineRenderer fixLine;
+    public LineRenderer hitNormalLine;
+    public float moveDistance;
+    public float hitCheckMovedDistance;
+}
