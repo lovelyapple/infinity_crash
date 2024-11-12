406e590feb3806ea8a63f82c60ad0e70b0c9a1da
lovelyapple <lovelyappleoffice@gmail.com>
2024-11-13 00:48:52 +0900

    ray test

--- Assets/PlayerControllerTest/RayTest.cs
+++ Assets/PlayerControllerTest/RayTest.cs
@@ -17,31 +17,33 @@ public class RayTest : MonoBehaviour
         }
 
 
-        var ray = new Ray(transform.position, (TargetTransform.position - transform.position).normalized);
-        LineRendererDir.SetPositions(new Vector3[] { transform.position, TargetTransform.position });
+        // var ray = new Ray(transform.position, (TargetTransform.position - transform.position).normalized);
+        // LineRendererDir.SetPositions(new Vector3[] { transform.position, TargetTransform.position });
 
-        if (Physics.SphereCast(ray, 0.5f, out var hitInfo, 50))
-        {
-            if (hitInfo.point == Vector3.zero)
-            {
-                if (Physics.Raycast(ray, out hitInfo, 50))
-                {
-                    LineRendererMove.SetPositions(new Vector3[] { transform.position, hitInfo.point, hitInfo.point + Vector3.up * 5f });
-                }
-                else
-                {
-                    LineRendererMove.SetPositions(new Vector3[] { transform.position, transform.position + ray.direction * 5 + Vector3.up * 5 });
-                }
-                return;
-            }
+        // if (Physics.SphereCast(ray, 0.5f, out var hitInfo, 50))
+        // {
+        //     if (hitInfo.point == Vector3.zero)
+        //     {
+        //         if (Physics.Raycast(ray, out hitInfo, 50))
+        //         {
+        //             LineRendererMove.SetPositions(new Vector3[] { transform.position, hitInfo.point, hitInfo.point + Vector3.up * 5f });
+        //         }
+        //         else
+        //         {
+        //             LineRendererMove.SetPositions(new Vector3[] { transform.position, transform.position + ray.direction * 5 + Vector3.up * 5 });
+        //         }
+        //         return;
+        //     }
 
-            LineRendererMove.SetPositions(new Vector3[] { transform.position, hitInfo.point, hitInfo.point + Vector3.up, hitInfo.point + Vector3.up + hitInfo.normal * 5f });
-            TargetPostTrasform.position = hitInfo.point + hitInfo.normal * 0.5f;
-        }
-        else
-        {
-            LineRendererMove.SetPositions(new Vector3[] { transform.position, transform.position + ray.direction * 5 + Vector3.up * 5 });
-        }
+        //     LineRendererMove.SetPositions(new Vector3[] { transform.position, hitInfo.point, hitInfo.point + Vector3.up, hitInfo.point + Vector3.up + hitInfo.normal * 5f });
+        //     TargetPostTrasform.position = hitInfo.point + hitInfo.normal * 0.5f;
+        // }
+        // else
+        // {
+        //     LineRendererMove.SetPositions(new Vector3[] { transform.position, transform.position + ray.direction * 5 + Vector3.up * 5 });
+        // }
+
+        CheckPhysic(TargetTransform.position - transform.position);
     }
     Vector3 gizmo1;
     Vector3 gizmo2;
@@ -124,10 +126,14 @@ public class RayTest : MonoBehaviour
         {
             moveDir2 = Vector3.zero;
         }
+
+        var ray2 = new Ray();
+        return Vector3.zero;
     }
-    void OnDrawGizmosSelected()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(gizmo1, 0.1f);
+        Gizmos.DrawWireSphere(gizmo2, 0.1f);
     }
 }
